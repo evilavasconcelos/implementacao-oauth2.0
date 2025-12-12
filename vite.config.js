232a0a6307e13daacf714e3197ae5bf7f38b1522
evilavasconcelos <evilamariaaraujo@gmail.com>
2025-12-12 16:02:52 -0300

    Reconfigurando a base do Vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/implementacao-ouauth2.0/", 
+  base: "/implementacao-oauth2.0/", 
 })
